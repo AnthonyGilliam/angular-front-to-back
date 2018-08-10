9c9216588c21df9c13caf974c48feaa40f517cce
Anthony <anthony.gilliam@gmail.com>
2018-08-10 17:27:13 -0400

    4:23 Template Syntax - Pipes & ngNonBindable

--- angularapp/src/app/app.module.ts
+++ angularapp/src/app/app.module.ts
@@ -2,16 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { TypesComponent } from './components/Types/types.component';
+import { TypesComponent } from './components/types/types.component';
 import { UserComponent } from './components/user/user.component';
 import { UsersComponent } from './components/users/users.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TypesComponent,
     UserComponent,
-    UsersComponent
+    UsersComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule
--- angularapp/src/app/components/users/users.component.ts
+++ angularapp/src/app/components/users/users.component.ts
@@ -12,17 +12,18 @@ export class UsersComponent implements OnInit {
   loaded: boolean;
   versionNum: number;
   siteImageSrc: string;
+  enableAdd: boolean = false;
+  currentClasses = {};
+  currentStyles: {};
 
   constructor() { }
 
   ngOnInit() {
     //Use property binding on the [src] attribute of the <img> tag to dynamically change an HTML image
     //Only 1 image will be loaded on the client per [src] binding! :)
-    this.versionNum = 5;
+    this.versionNum = 6;
     this.siteImageSrc = this.versionNum == 5 ? "../../../assets/angular5.png" : "../../../assets/angular6.png";
-
     this.showExtended = true;
-
     this.loaded = false;
 
     setTimeout(() => {
@@ -35,7 +36,11 @@ export class UsersComponent implements OnInit {
             street: '50 Main st',
             city: 'Boston',
             state: 'MA'
-          }
+          },
+          image: 'https://loremflickr.com/600/600/brazil,guy',
+          isActive: true,
+          balance: 100,
+          registered: new Date('01/02/2018 08:30:00')
         },
         {
           firstName: 'Kevin',
@@ -45,7 +50,11 @@ export class UsersComponent implements OnInit {
             street: '20 School st',
             city: 'Lynn',
             state: 'MA'
-          }
+          },
+          image: 'https://loremflickr.com/600/600/paris,guy',
+          isActive: false,
+          balance: 200,
+          registered: new Date('01/02/2018 08:30:00')
         },
         {
           firstName: 'Karen',
@@ -55,7 +64,11 @@ export class UsersComponent implements OnInit {
             street: '55 Mill st',
             city: 'Miami',
             state: 'FL'
-          }
+          },
+          image: 'https://loremflickr.com/600/600/brazil,girl',
+          isActive: true,
+          balance: 50,
+          registered: new Date('11/02/2016 10:30:00')
         }
       ];
 
@@ -65,6 +78,8 @@ export class UsersComponent implements OnInit {
       });
 
       this.loaded = true;
+      this.setCurrentClasses();
+      this.setCurrentStyles();
     }, 2000);
   }
 
@@ -72,4 +87,18 @@ export class UsersComponent implements OnInit {
     this.users.push(user);
   }
 
+  setCurrentClasses() {
+    this.enableAdd = true;
+    this.currentClasses = {
+      'btn-success': this.enableAdd,
+      'big-text': this.showExtended
+    }
+  }
+
+  setCurrentStyles() {
+    this.currentStyles = {
+      'padding-top': this.showExtended ? '40px' : 0,
+      'font-size': this.showExtended ? '' : '40px'
+    }
+  }
 }
