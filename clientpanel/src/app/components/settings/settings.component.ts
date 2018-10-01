864efa587e8ba358f9e2ed2745367295de95b84b
Anthony <anthony.gilliam@gmail.com>
2018-10-01 04:36:48 -0400

    10:73 Client Panel Project - Part 2 - Deploy To Firebase Hosting (course complete! :)

--- clientpanel/src/app/components/settings/settings.component.ts
+++ clientpanel/src/app/components/settings/settings.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+
+import { Settings } from '../../models/Settings';
+import { SettingsService } from '../../services/settings.service';
 
 @Component({
   selector: 'app-settings',
@@ -6,10 +11,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./settings.component.css']
 })
 export class SettingsComponent implements OnInit {
+  settings: Settings;
 
-  constructor() { }
+  constructor(private router: Router,
+              private toastr: ToastrService,
+              private settingsService: SettingsService) { }
 
   ngOnInit() {
+    this.settings = this.settingsService.getSettings();
   }
 
+  onSubmit() {
+    this.settingsService.changeSettings(this.settings);
+    this.toastr.success('Settings saved!', 'Success!');
+  }
 }
